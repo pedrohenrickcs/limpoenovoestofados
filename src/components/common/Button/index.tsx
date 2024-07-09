3f03d70eca3ea9800edf4e1ae975b2c380e22ba1
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-09 13:44:35 -0300

    feat: 🎸 wpp

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -4,7 +4,7 @@ const Button = () => {
   return ( 
     <div className="py-8">
       <a className="px-8 py-4 min-w-[160px] inline-flex text-center text-white bg-violet-600 border border-violet-600 rounded active:text-violet-500 hover:bg-white hover:text-primary-bg-color focus:outline-none focus:ring transition-colors"
-        href="#">
+        href="https://wa.link/adczed" target="_blank">
         <Whatsapp />
         <span className="pl-2">Solicite um orçamento</span>
       </a>
