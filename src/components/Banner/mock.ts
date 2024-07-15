b402cbedd3cc378163b52322dfc890bb23027069
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-15 11:31:18 -0300

    feat: 🎸 images

--- src/components/Banner/mock.ts
+++ src/components/Banner/mock.ts
@@ -2,6 +2,6 @@ export const items = [
   {
     title: 'Higienização Completa e Impermeabilização de Estofados',
     description: 'Tenha um estofado limpo e novo e toda sua família protegida das impurezas',
-    image: '/banner_v5.jpg',
+    image: './banner_v5.jpg',
   },
 ]
--- src/components/Infos/mock.ts
+++ src/components/Infos/mock.ts
@@ -4,23 +4,23 @@ export const items = [
     description: 'Os estofados são uma parte essencial de nossas casas e escritórios, proporcionando conforto e estilo aos nossos ambientes. No entanto, muitas vezes negligenciamos a necessidade de mantê-los limpos e higienizados. A higienização regular dos estofados é crucial por várias razões',
     itemsInfos: [
         {
-            icon: '/check.svg',
+            icon: './check.svg',
             title: 'Saúde e Bem-Estar',
         },
         {
-            icon: '/check.svg',
+            icon: './check.svg',
             title: 'Durabilidade e Aparência',
         },
         {
-            icon: '/check.svg',
+            icon: './check.svg',
             title: 'Eliminação de Odores',
         },
         {
-            icon: '/check.svg',
+            icon: './check.svg',
             title: 'Prevenção de Infestações',
         },
         {
-            icon: '/check.svg',
+            icon: './check.svg',
             title: 'Adequado para uso em ambientes comerciais',
         },
     ]
