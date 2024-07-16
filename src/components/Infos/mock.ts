34232303cc2c37f09c026820ed9ebcfa4beca22e
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-16 15:31:31 -0300

    feat: 🎸 image

--- src/components/Infos/mock.ts
+++ src/components/Infos/mock.ts
@@ -5,23 +5,23 @@ export const infos = [
       'Os estofados são uma parte essencial de nossas casas e escritórios, proporcionando conforto e estilo aos nossos ambientes. No entanto, muitas vezes negligenciamos a necessidade de mantê-los limpos e higienizados. A higienização regular dos estofados é crucial por várias razões',
     itemsInfos: [
       {
-        icon: './check.svg',
+        icon: '/check.svg',
         title: 'Saúde e Bem-Estar',
       },
       {
-        icon: './check.svg',
+        icon: '/check.svg',
         title: 'Durabilidade e Aparência',
       },
       {
-        icon: './check.svg',
+        icon: '/check.svg',
         title: 'Eliminação de Odores',
       },
       {
-        icon: './check.svg',
+        icon: '/check.svg',
         title: 'Prevenção de Infestações',
       },
       {
-        icon: './check.svg',
+        icon: '/check.svg',
         title: 'Adequado para uso em ambientes comerciais',
       },
     ],
