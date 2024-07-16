9db74f78c80256e969d174baf49b0105f5d3d82d
Pedro Henrick <pedro.henrickcs@gmail.com>
2024-07-15 22:23:45 -0300

    feat: 🎸 em construção

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,14 @@
 'use client'
 
-import { Banner } from '@/components/Banner'
-import Infos from '@/components/Infos'
-import Services from '@/components/Services'
-import Header from '@/components/layout/Header'
-
-import { items } from '@/components/Banner/mock'
-import { infos, whoUs } from '@/components/Infos/mock'
-import { itemsServices } from '@/components/Services/mock'
 export default function Home() {
-  const bannerMock = items
-  const infosMock = infos
-
   return (
     <main className="flex flex-col items-center justify-between">
-      <Header />
+      Em construção... 😊
+      {/* <Header />
       <Banner items={bannerMock} />
       <Infos items={infosMock} benefits={true} id="benefits" />
       <Services items={itemsServices} id="services" />
-      <Infos items={whoUs} contentImage={true} id="who-us" />
+      <Infos items={whoUs} contentImage={true} id="who-us" /> */}
     </main>
   )
 }
